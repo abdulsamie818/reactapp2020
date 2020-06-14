29c8338ac33b96b4241bfbdf248f6bcd513da3da
Abdul Samie <38508100+samamico@users.noreply.github.com>
2020-06-14 16:33:12 +0200

    üpdate using context to work with usestate

--- src/Child.js
+++ src/Child.js
@@ -4,9 +4,13 @@ import ValueContext from './ValueContext';
 
 function Child() {
   let value = useContext(ValueContext)
+  let updateValue = value[1];
+  let incValue = value[0];
   return (
     <div>
-    Child number {value}
+    Child number {incValue}
+    <br/>
+    <button onClick = {() => {updateValue (++incValue)}}>Update Value</button>
     </div>
   );
 }
